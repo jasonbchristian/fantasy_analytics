045c582b3230f4457e893a4e263c56955b8ca567
jason <jasonchristian94@yahoo.com>
2026-06-04 01:00:37 +0200

    reformatted get_depth into a function call

--- get_depth.py
+++ get_depth.py
@@ -1,23 +1,38 @@
 import nflreadpy as nfl
 import polars as pl
-import pandas as pd
 
-depth = nfl.load_depth_charts()
-cols = ["team", "player_name", "pos_abb", "pos_rank"]
-fantasy_pos = ["QB","RB", "WR", "TE"]
-team = ["TB"]
-# print(depth.glimpse())
+# most recent depth chart for all NFL teams
 
+def get_depth(position = None, team = None, depth = None):
+    #defines relevant columns and positions
+    if position == None:
+        position = ["QB", "WR", "RB", "TE"]
+    cols = ["team", "player_name", "gsis_id", "pos_abb", "pos_rank"]
 
+    #loads full depth chart and finds most recent version
+    full_depth = nfl.load_depth_charts()
+    # The .item() belongs here, after the selection
+    latest_depth = full_depth.select(pl.col("dt").max()).item()
 
-# positions = depth.filter(pl.col("pos_abb").is_in(fantasy_pos) & pl.col("team").is_in(team))
-positions = depth.filter(pl.col("pos_abb").is_in(fantasy_pos))
-latest_date = positions.select(pl.col("dt").max()).item()
+    #filters down to relevant positions and most recent depth chart
+    depth_chart = (
+        full_depth.filter(
+            (pl.col("pos_abb").is_in(position)) &
+            (pl.col("dt") == latest_depth)
+        )
+        .select(pl.col(cols))
+        .sort(["team", "pos_abb", "pos_rank"])
+    )
+    
+    #pulls depth for specified team/s
+    if team is not None:
+        depth_chart = depth_chart.filter(pl.col("team").is_in(team))
+    
+    #filters to only pull through depth = d
+    if depth is not None:
+        depth_chart = depth_chart.filter(pl.col("pos_rank") <= depth)
+    return depth_chart
 
-positions = positions.filter(
-    (pl.col("dt") == latest_date) & 
-    (pl.col("pos_rank") <= 3))
-
-positions = positions.select(pl.col(cols)).sort(["team", "pos_abb", "pos_rank"])
-
-positions.write_csv("depth.csv")
+res = get_depth(position=["WR"], depth=4)
+res.write_csv("depth.csv")
+print("Depth chart successfully pulled!")
