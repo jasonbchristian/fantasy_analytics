bd016cba5cc87bbbcfc62c923e4d4f640d55ee45
jason <jasonchristian94@yahoo.com>
2026-06-03 03:23:35 +0200

    pull player stats function

--- get_fantasy_rankings.py
+++ get_fantasy_rankings.py
@@ -7,6 +7,6 @@ fantasy_pos = ["QB","RB", "WR", "TE"]
 
 print(ff_rank.glimpse())
 
-ff_rank = ff_rank.filter((pl.col("team")=="HOU") & pl.col("pos").is_in(fantasy_pos))
+ff_rank = ff_rank.filter((pl.col("team")=="TB") & pl.col("pos").is_in(fantasy_pos))
 
 ff_rank.write_csv("ff_rankings.csv")
--- player_season.py
+++ player_season.py
@@ -1,18 +1,25 @@
 import nflreadpy as nfl
 import polars as pl
 import pandas as pd
-import Download
+
 
 player = "Patrick Mahomes"
 year = 2025
 week = 15
+fantasy_pos = ["QB","RB", "WR", "TE"]
 
 player_stats = nfl.load_player_stats(seasons=[year])
 
 # print(player_stats.describe())
 # print(player_stats.glimpse())
 
-target_stats = player_stats.filter(pl.col("player_display_name") == player)
-print(target_stats)
+target_stats = (
+    player_stats.filter(pl.col("position").is_in(fantasy_pos))
+    .with_columns(
+        game_count = pl.len().over("player_id")
+    )
+    .filter(pl.col("game_count") >= 10)
+    .drop("game_count")
+    )
 
-print(target_stats.glimpse())
+target_stats.write_csv("player_season.csv")
